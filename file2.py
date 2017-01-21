8cfb3712e8080db2877a0d4cb92cef88a0d8d562
Mihir Trivedi <mihirtrivedigm@yahoo.in>
2017-01-21 11:02:18 +0530

    Update file2.py

--- file2.py
+++ file2.py
@@ -2,10 +2,14 @@ import os
 
 def file_program():
     list_of_files = os.listdir(r"D:\Mihir Study\white panda")
+    saved_path = os.getcwd()
+    print("Current working directory is: "+saved_path)
+    os.chdir(r"D:\Mihir Study\white panda")
     print(list_of_files)
+    for file_name in list_of_files:
+        a = file_name.translate(None, "0123456789")
+        os.rename(file_name, a)
+    os.chdir(r"D:\Mihir Study\white panda")
 
 
-for file_name in list_of_file
-    a = file_name.translate
-    os.rename(file_name,a)
-#file_program()
+file_program()
